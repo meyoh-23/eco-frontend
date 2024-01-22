46194290a8d5f8ab8aacde47ee15d1154eb8ad6b
Felix Ouma <moxiemeyoh@gmail.com>
2024-01-22 09:05:12 +0300

    Forntend setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-400">
+          <p className="text-blue-400">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptas rem officia suscipit repellat tenetur amet, nobis itaque illo velit delectus optio dicta hic perspiciatis repellendus in, obcaecati nihil cupiditate!
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default App
